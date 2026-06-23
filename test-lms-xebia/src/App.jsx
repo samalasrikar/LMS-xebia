31a5efb5cf67959dd4d88c6fc7926974b6fcb3a5
samala srikar <148432583+samalasrikar@users.noreply.github.com>
2026-06-23 22:10:41 +0530

    Frontend base setup with routing and shadcn

--- test-lms-xebia/src/App.jsx
+++ test-lms-xebia/src/App.jsx
@@ -1,7 +1,12 @@
-import AppRoutes from "./routes/AppRoutes";
+import { Button } from "@/components/ui/button";
 
 function App() {
-  return <AppRoutes />;
+  return (
+    <div className="p-10">
+      <h1 className="text-3xl font-bold mb-4">LMS Frontend</h1>
+      <Button>Shadcn Working</Button>
+    </div>
+  );
 }
 
 export default App;
