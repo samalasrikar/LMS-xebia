4c083026ec54cbbdcc2e998d736f364df7efbf28
samala srikar <148432583+samalasrikar@users.noreply.github.com>
2026-06-24 11:19:36 +0530

    Delete test-lms-xebia/dashboard-app directory

--- test-lms-xebia/dashboard-app/src/pages/Courses.tsx
+++
@@ -1,4 +0,0 @@
-export default function Courses() {
-  return <div className="p-8"><h1>Courses View</h1></div>;
-}
-
--- test-lms-xebia/dashboard-app/src/pages/Dashboard.tsx
+++
@@ -1,3 +0,0 @@
-export default function Dashboard() {
-  return <div className="p-8"><h1>Dashboard View</h1></div>;
-}
